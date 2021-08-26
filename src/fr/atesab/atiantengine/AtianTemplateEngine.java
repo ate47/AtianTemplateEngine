33922e2cd75cbc6a46903dd6927255428e177959
ATE47 <wilate@yahoo.fr>
2021-08-26 15:48:57 +0200

    remove unused imports

--- src/fr/atesab/atiantengine/AtianTemplateEngine.java
+++ src/fr/atesab/atiantengine/AtianTemplateEngine.java
@@ -1,8 +1,6 @@
 package fr.atesab.atiantengine;
 
-import java.io.BufferedReader;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
